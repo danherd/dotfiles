f49f4ef036fa5011c7526f56d4321f58e89de0a8
Dan Herd <dev@danherd.com>
2023-10-03 16:43:47 +0100

    chore: add logfile

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Last run: $(date)\n" >install.log
+
 THIS_DIR="$(dirname "$(readlink -f "$0")")"
 
 echo "\nDan's Mac Install Script"
@@ -8,7 +10,7 @@ echo "************************\n"
 printf "Checking if Oh My Zsh is installed..."
 if ! [ -d "$HOME/.oh-my-zsh" ]; then
   printf " Not installed - installing..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >/dev/null 2>&1
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" >>install.log 2>&1
   echo " Done"
 else echo " Already installed"; fi
 
@@ -16,19 +18,19 @@ printf "Checking if Homebrew is installed..."
 if ! [ "$(command -v brew)" ]; then
   printf " Not installed - installing (might prompt for sudo password)..."
   sudo true
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" >/dev/null 2>&1
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" >>install.log 2>&1
   echo " Done"
 else echo " Already installed"; fi
 
 printf "Installing / updating Homebrew packages..."
-brew upgrade -q >/dev/null 2>&1
-brew install -q python@3.11 git awscli nano nanorc ripgrep archey4 >/dev/null 2>&1
+brew upgrade -q >>install.log 2>&1
+brew install -q python@3.11 git awscli nano nanorc ripgrep archey4 >>install.log 2>&1
 echo " Done"
 
 printf "Checking if NVM is installed..."
 if ! [ -d "$HOME/.nvm" ]; then
   printf " Not installed - installing..."
-  curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash >/dev/null 2>&1
+  curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash >>install.log 2>&1
   echo " Done"
 else echo " Already installed"; fi
 
